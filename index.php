1318464fabeca583c870591dee6e065c0f0a8c12
Greykom <91327449+Greykom@users.noreply.github.com>
2021-09-25 19:24:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,4 @@
         <h1>Привет мир !!</h1>
         <p>Привет мир !</p>
     </body>
-<script type="text/javascript" nonce="270cc42346d84f58b00eeb6325e" src="//local.adguard.org?ts=1632556660303&amp;type=content-script&amp;dmn=greykom.site&amp;app=chrome.exe&amp;css=1&amp;js=1&amp;gcss=1&amp;rel=1&amp;rji=1&amp;sbe=0"></script>
-<script type="text/javascript" nonce="270cc42346d84f58b00eeb6325e" src="//local.adguard.org?ts=1632556660303&amp;name=AdGuard%20Popup%20Blocker&amp;name=AdGuard%20Extra&amp;type=user-script"></script></head>
 </html>
